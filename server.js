5f49b9b2310c592a2cde57ae718f39605d93c11c
Johnson Han <hancanada@hotmail.com>
2016-09-06 22:33:39 -0400

    favicon fix

--- server.js
+++ server.js
@@ -4,19 +4,15 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 users = [];
-connections = [];
+connections = []; 
 
-
- 
- 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html');
+  response.sendFile(__dirname + '/index.html'); 
 });
 
 
 //Port Settings
-app.set('port', (process.env.PORT || 1000));
-
+app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
