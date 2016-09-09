0ce1f2491270157eaf1ab6d8b778401f85824971
Johnson Han <hancanada@hotmail.com>
2016-09-08 23:03:20 -0400

    Front End: Channeling two Scaffolding

--- js/script.js
+++ js/script.js
@@ -8,16 +8,42 @@ var backgroundImageFlashingOrder = 0;
 //logic
 window.onload = function(){
   $('.user-name').html(firstname + space + lastname);
-  master_state_change(1);
+  master_state_change(2);
   console.log('Ready');
   colorSwitch();
   var backgroundImageFlashing = setInterval(function(){
       colorSwitch();
 	}, 5000);
   complete_canvas();
+  loadChannel(true);
+  //key stroke
+  document.getElementsByTagName('body')[0].onkeydown = function(e) {
+      var ev = e || event;
+      var key = ev.keyCode;
+      if(ev.keyCode == 13) {
+        console.log('Enter Pressed');
+        if(current_user_state == 1 || current_user_state == 2){
+            zoneTravel(current_user_state);
+        }
+      }
+   }
 };
 
 function zoneTravel(current_zone){
+  //validation
+  if(current_zone == 1){
+    if($('.username-input').val() == ''){
+        system(true,'Name cannot be empty!');
+        return;
+    }
+  }else if(current_zone == 2){
+
+  }
+  else{
+    system(true,'Invalid Zone Travel');
+    return;
+  }
+  //actions
   current_zone++;
   if(current_zone > 3) current_zone -= 3;
   master_state_change(current_zone);
@@ -35,16 +61,24 @@ function master_state_change(newState){
         // $('.background-image').addClass('hidden');
         $('.container-register').removeClass('hidden');
         $('.container-chat').css('transform','rotateY(180deg) rotateX(180deg)');
+        $('.container-chat').css('width','0px');
+        $('.container-chat').css('height','0px');
         break;
     case 2:
         // $('.background-image').addClass('hidden');
         $('.container-channel').removeClass('hidden');
         $('.container-chat').css('transform','rotateY(180deg) rotateX(180deg)');
+        $('.container-chat').css('width','0px');
+        $('.container-chat').css('height','0px');
         break;
     case 3:
         // $('.background-image').removeClass('hidden');
         $('.container-chat').removeClass('hidden');
-        setTimeout(function(){$('.container-chat').css('transform','rotateY(0deg) rotateX(0deg)'); }, 50);
+        setTimeout(function(){
+            $('.container-chat').css('transform','rotateY(0deg) rotateX(0deg)');
+            $('.container-chat').css('width','80vw');
+            $('.container-chat').css('height','100vh');
+        }, 50);
         break;
     default:
         var message = 'Warning: internal system error';console.log(message);  alert(message);
@@ -55,6 +89,9 @@ function master_state_change(newState){
 function colorSwitch(){
   $(".background-image").css('background-color',rainbow[backgroundImageFlashingOrder]);
   $(".register-instruction").css('color',rainbow[arrayIncrease(3)]);
+  $(".username-input").css('background-color',rainbow[arrayIncrease(3)]);
+  $(".username-input").css('color',rainbow[backgroundImageFlashingOrder]);
+  $(".container-register-connection").css('border-top-color',rainbow[arrayIncrease(3)]);
   $(".username-input").css('border-left-color',rainbow[arrayIncrease(3)]);
   $(".username-input").css('border-top-color' ,rainbow[arrayIncrease(4)]);
   $(".username-input").css('border-right-color',rainbow[arrayIncrease(5)]);
@@ -85,4 +122,28 @@ function complete_canvas(){
   ctx.lineTo(c.width / 2, c.height);
   ctx.fillstyle = arrayIncrease(3);
   ctx.fill();
+}
+function showSystemMessage(decision){
+  if($('.system').width() > 0 && decision == true )return;
+  else if(decision == false){
+    $('.system').css('width','0px');
+    setTimeout(function(){$('.system').addClass('hidden');}, 1000);
+  }
+  else if(decision == true){
+      $('.system').removeClass('hidden');
+      setTimeout(function(){$('.system').css('width','200px');}, 10);
+      setTimeout(function(){showSystemMessage(false);}, 2000);
+  }
+}
+function setSystemMessage(message){
+  $('.system').html(message);
+}
+function system(decision,message){
+  setSystemMessage(message);
+  showSystemMessage(decision);
+}
+function loadChannel(decision){
+    if(decision == true){
+
+    }
 }
