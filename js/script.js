cf4f1a13321547572de821819b67e5ca24a54a9d
Johnson Han <hancanada@hotmail.com>
2016-09-23 14:45:58 -0400

    Google Analytics

--- js/script.js
+++ js/script.js
@@ -253,3 +253,12 @@ function showLatestMessage(){// show latest Message
 function clearMessages(){// clear all messages from channel
     $('.chat-window').html('');
 }
+
+// Google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-84695208-1', 'auto');
+ga('send', 'pageview');
