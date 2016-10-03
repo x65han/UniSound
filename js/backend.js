72d8403418e23b2dbe5f5775a4c92889bd90b9f9
Johnson Han <hancanada@hotmail.com>
2016-10-02 22:02:46 -0400

    LocationManager Responsible to machine learn User behavior

--- js/backend.js
+++ js/backend.js
@@ -13,7 +13,6 @@ function sendMessage(){
     if(hackerWatch < 0.5){system(true, "Too Fast");$('#message').val('');return;}
     console.log('sending message');
     var message_wrapper = {
-        'channel': current_user_channel,
         'message': $('#message').val().trim(),
         'signature': username
     };
@@ -25,7 +24,7 @@ function sendMessage(){
 }
 function establishConnection(){
     socket = io.connect("");
-    socket.emit("request channels",username);
+    socket.emit("request channels", username);
     socket.on('new message', function(data){
         console.log('SOCKET Received new message from:  ' + data.signature + " in channel: " + data.channel);
         console.log("Message Detail: " + data.message);
@@ -34,14 +33,14 @@ function establishConnection(){
         showLatestMessage();
     });
     socket.on('force update', function(data){
-        if(current_user_state == 3){
+        // if(current_user_state == 3){
             system(true, 'Reloading messages');
             current_user_state = 2;
             clearMessages();
             environmentMessageReady = false;
             getAndLoadMessageFromChannel();
             current_user_state = 3;
-        }
+        // }
     });
     socket.on('distribute channels', function(data){
         if(environmentSetup != 3){
@@ -68,4 +67,17 @@ function registerUserWithServer(username){
 }
 function getAndLoadMessageFromChannel(){//Navigate to according channel & grab specific channel detail
     socket.emit("request channel message",current_user_channel);
+}
+function createNewChannel(newChannelName){
+    console.log("Request New Channel: " + newChannelName);
+    if(newChannelName == null || newChannelName == undefined || newChannelName =="" || typeof newChannelName != typeof "z"){
+        return;
+    }
+    if(channels.indexOf(newChannelName) == -1){
+        socket.emit("create new channel", newChannelName);
+        channels.push(newChannelName);
+        loadChannels(true);
+        return;
+    }
+    console.log("Channels Exists: " + newChannelName);
 }
